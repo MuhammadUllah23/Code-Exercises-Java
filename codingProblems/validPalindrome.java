ce2b3b4c730db0dd3848e219a9d3b02e68c46790
M.Ullah <muhammadhullah23@gmail.com>
2026-01-25 22:24:34 -0500

    validPalindrome solution

---
+++ codingProblems/validPalindrome.java
@@ -0,0 +1,29 @@
+package codingProblems;
+
+public class validPalindrome {
+        public static void main (String[] args) { 
+        System.out.println(isPalindrome("Was it a car or a cat I saw?"));
+        System.out.println(isPalindrome("tab a cat"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        // First Step remove spaces and special characters from string
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        // Second Step Use two pointers to check letters on each end match. 
+        // Third Step if letters do not match then return false fast
+        int i = 0;
+        int j = str.length() - 1;
+        while (i <= j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        
+        // once two pointers is finished then return true
+        return true;
+    }
+
+}
