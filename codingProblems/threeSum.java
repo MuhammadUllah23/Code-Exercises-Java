90f74c7311b0bcba92e6733a1466f7c1f0b78ae1
M.Ullah <muhammadhullah23@gmail.com>
2026-01-27 17:11:50 -0500

    fix threeSum solution

--- codingProblems/threeSum.java
+++ codingProblems/threeSum.java
@@ -7,23 +7,22 @@ import java.util.List;
 public class threeSum {
         public static void main (String[] args) { 
         // Basic Case
-        System.out.println(threeSumSolution(List.of(2,7,11,15)));
-        // Negative Numbers
-        System.out.println(threeSumSolution(List.of(-3,4,3,90)));
-        // Duplicate Values
-        System.out.println(threeSumSolution(List.of(3,3)));
-        // Larger Array
-        System.out.println(threeSumSolution(List.of(1,5,7,2,8)));
-        // Mixed Positive Negative Numbers
-        System.out.println(threeSumSolution(List.of(0,4,3,0)));
+        // System.out.println(threeSumSolution(new int[]{-1,0,1,2,-1,-4}));
+        // // Negative Numbers
+        // System.out.println(threeSumSolution(new int[]{0,1,1}));
+        // // Duplicate Values
+        // System.out.println(threeSumSolution(new int[]{1,-1,-1,0}));
+
+        System.out.println(threeSumSolution(new int[]{-2,0,1,1,2}));
     }
-    
+    // -2, 0, 1, 1, 2
     public static List<List<Integer>> threeSumSolution(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
         if(nums.length < 3) return result;
 
         Arrays.sort(nums);
+        
 
         for(int i = 0; i < nums.length-2; i++) {
             if(nums[i] > 0) break;
@@ -33,14 +32,22 @@ public class threeSum {
 
             int left = i + 1;
             int right = nums.length - 1;
+            System.out.println("i: ");
+            System.out.println(i);
 
             while(left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
+                System.out.println("left: ");
+                System.out.println(nums[left]);           
+                System.out.println("right: ");
+                System.out.println(nums[right]);
+
+                System.out.println("sum: ");
+                System.out.println(sum);
 
                 // if sum is 0 then add to result and shift left/right
                 // if sum is greater than 0 then shift left
                 // if sum is less than 0 than shift right
-
                 if(sum == 0) {
                     result.add(List.of(nums[i], nums[left], nums[right]));
 
@@ -48,12 +55,12 @@ public class threeSum {
                     right--;
 
                     // skip duplicates
-                    if(left < right && nums[left] == nums[left+1]) left++;
-                    if(left < right && nums[right] == nums[right-1]) right--; 
+                    if(left < right && nums[left] == nums[left-1]) left++;
+                    if(left < right && nums[right] == nums[right+1]) right--; 
 
-                } else if(sum > 0) {
-                    left++;
                 } else if(sum < 0) {
+                    left++;
+                } else if(sum > 0) {
                     right--;
                 }
 
