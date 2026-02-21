6a77320bbbba6b74d64045711cdf77bdc245840e
M.Ullah <muhammadhullah23@gmail.com>
2026-02-20 22:49:17 -0500

    validParenthesis solution

---
+++ codingProblems/stack/validParenthesis.java
@@ -0,0 +1,38 @@
+package codingProblems.stack;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class validParenthesis {
+    public static void main(String[] args) {
+        System.out.println(isValid("[]"));
+        System.out.println(isValid("([{}])"));
+        System.out.println(isValid("[(])"));
+        System.out.println(isValid("["));
+    }
+
+    public static Boolean isValid(String s) {
+        
+        Stack<String> stack = new Stack<>();
+        List<String> open = List.of("(", "{", "[");
+        Map<String, String> map = Map.of("(", ")", "{", "}", "[", "]");
+        
+        for(char c : s.toCharArray()) {
+            String curr = String.valueOf(c);
+            if (open.contains(curr)) {
+                stack.push(curr);
+            } else {
+                if (stack.isEmpty()) return false;
+                String popped = stack.pop();
+                if(!curr.equals(map.get(popped))) {
+                    return false;
+                }
+            }
+  
+        }
+        if (!stack.isEmpty()) return false;
+        return true;
+    }
+}
