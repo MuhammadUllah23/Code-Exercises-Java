56977f2948e586e7aebadec7bbd5fdb2c9ae27c1
M.Ullah <muhammadhullah23@gmail.com>
2026-04-03 17:02:06 -0400

    add binary solution

---
+++ codingProblems/AddBinary.java
@@ -0,0 +1,36 @@
+package codingProblems;
+
+public class AddBinary {
+
+    public static void main(String[] args) {
+        System.out.println(addBinary("11", "1"));
+        System.out.println(addBinary("1010", "1011"));
+    }
+
+    public static String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            carry = sum / 2;
+            result.append(sum % 2);
+        }
+
+        return result.reverse().toString();
+    }
+    
+}
