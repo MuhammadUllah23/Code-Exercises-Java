359b6899da6bfbd4bcddeb8ede0c2380065fcefc
M.Ullah <muhammadhullah23@gmail.com>
2026-04-23 22:53:53 -0400

    topKFrequent practice

--- codingProblems/ArrayAndHashing/topKFrequent.java
+++ codingProblems/ArrayAndHashing/topKFrequent.java
@@ -8,27 +8,38 @@ import java.util.PriorityQueue;
 public class topKFrequent {
     public static void main(String[] args) {
         System.out.println(topKFrequentSolution(new int[]{1,2,2, 4, 4, 4, 4, 3,3,3}, 2));
-        System.out.println(topKFrequentSolution(new int[]{7,7}, 1));
+        // System.out.println(topKFrequentSolution(new int[]{7,7}, 1));
     }
 
     public static int[] topKFrequentSolution(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int n : nums) {
-            freq.put(n, freq.getOrDefault(n, 0) + 1);
+        // insert into hashmap value as key and its frequency in the array.
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> heap =
-                new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        // create new priority queue with comparing value of of map::getValue
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
 
-        for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
-            heap.offer(e);
-            if (heap.size() > k) heap.poll(); 
+        // iterate through map
+        for(Map.Entry<Integer, Integer> entry : map.entrySet() ) {
+
+            // add entry to priority queue
+            queue.add(entry);
+
+            // if queue size > k then poll
+            if(queue.size() > k) {
+                queue.poll();
+            }
         }
 
+        // put values from queue to array
         int[] res = new int[k];
         for (int i = k - 1; i >= 0; i--) {   
-            res[i] = heap.poll().getKey();
+            res[i] = queue.poll().getKey();
         }
         return res;
     }
+
 }
